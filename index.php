fd94a9f4fdf8f2e7e3d8d7313488e04d9703c92f
harusame1018 <163406621+harusame1018@users.noreply.github.com>
2024-07-04 00:31:01 +0000

    edit

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <script>
+            const main = document.body;
+            let text = document.createElement("p");
+            text.innerText("hello javascript");
+            window.onload = function() {
+                main.appendChild(text);
+            }
+        </script>
     </head>
     <body>
         <h1>へっぉをｒｌｄ</h1>
